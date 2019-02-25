2f1d9e8a1769d7d5baf5fa03f86c124ea853c633
Myles J <hi@myles.ninja>
2019-02-25 13:11:17 +0000

    chore: jest config - support coveralls

--- test/getIterator.test.js
+++ test/getIterator.test.js
@@ -1,4 +1,5 @@
-const { getIterator, withIterator } = require('..')
+import { getIterator } from '../src/util'
+import { withIterator } from '../src/withIterator'
 
 describe('getIterator', () => {
 	describe('when passed an object with an iterator', () => {
@@ -6,11 +7,13 @@ describe('getIterator', () => {
 			const obj = { [Symbol.iterator]: true }
 			expect(getIterator(obj)).toBe(undefined)
 		})
+
 		test('should return a function if it exists', () => {
 			const obj = withIterator({})
 			expect(typeof getIterator(obj)).toBe('function')
 		})
 	})
+
 	describe('when passed a function', () => {
 		test('should return iterators on a constructable prototype', () => {
 			class Test {}
@@ -18,27 +21,32 @@ describe('getIterator', () => {
 			withIterator(iterator, Test.prototype)
 			expect(getIterator(Test)).toBe(iterator)
 		})
+
 		test('should prioritise own properties even if erroneous', () => {
 			class Test {}
 			withIterator(Test.prototype)
 			Test[Symbol.iterator] = true
 			expect(getIterator(Test)).toBe(undefined)
 		})
+
 		test('should not be perturbed by non-constructables', () => {
 			expect(getIterator(() => {})).toBe(undefined)
 		})
 	})
+
 	describe('when passed an iterable', () => {
 		test('should return a working iterator', () => {
 			const defaultIterator = getIterator(withIterator(undefined))
 			const str = withIterator(defaultIterator, 'string')
 			expect(Array.from(str)[0]).toBe('string')
 		})
+
 		test('should work with primitives', () => {
 			const iterator = getIterator('string')
 			const num = withIterator(iterator, 42)
 			expect(Array.from(num)).toEqual(['4', '2'])
 		})
+
 		test('should work with native prototypes', () => {
 			const iterator = getIterator(String)
 			const num = withIterator(iterator, 42)
